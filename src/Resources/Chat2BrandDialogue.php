974a314a7aec2edac6d1dbb15508746815077471
Alexander <alexg@infotechred.co.za>
2018-04-25 10:30:05 +0200

    state to chatdialogue

--- src/Resources/Chat2BrandDialogue.php
+++ src/Resources/Chat2BrandDialogue.php
@@ -6,6 +6,9 @@ use TimuTech\Chat2Brand\Resources\Abstracts\ChatClient;
 
 class Chat2BrandDialogue
 {
+    const STATE_OPEN = 'open';
+    const STATE_CLOSED = 'closed';
+
     protected $id;
     protected $state;
     protected $operator_id;
@@ -55,4 +58,30 @@ class Chat2BrandDialogue
     {
         return $this->operator_id;
     }
+
+    public function fill(array $data)
+	{
+		$this->id = $data['id'];
+        $this->state = isset($data['state']) ? $data['state'] : null;
+        $this->operator_id = isset($data['operator_id']) ? $data['operator_id'] : null;
+        $this->begin = isset($data['begin']) ? Carbon::parse($data['begin']) : null;
+        $this->end = isset($data['end']) ? Carbon::parse($data['end']) : null;
+
+		return $this;
+	}
+
+	public function __toString()
+	{
+		return serialize($this);
+	}
+
+	protected static function isAssociative(array $array)
+	{
+	    // Keys of the array
+	    $keys = array_keys($array);
+
+	    // If the array keys of the keys match the keys, then the array must
+	    // not be associative (e.g. the keys array looked like {0:0, 1:1...}).
+	    return array_keys($keys) !== $keys;
+	}
 }
